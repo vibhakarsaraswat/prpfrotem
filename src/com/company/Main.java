c711e7d4726944bd92b9b74ace0ae7f37faa4dbe
vibhakarsaraswat <vibhakar.saraswat@gmail.com>
2016-12-16 06:57:49 +0100

    01: MT - added a new method  - Hej

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,20 @@ package com.company;
 class Hi extends Thread {
     public void run() {
         for(int i = 1; i <= 5; i++) {
-            System.out.println("Hi");
+            System.out.println("1 - Hi");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class Hej extends Thread {
+    public void run() {
+        for(int i = 1; i <= 5; i++) {
+            System.out.println("2 - Hej");
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
@@ -16,7 +29,7 @@ class Hi extends Thread {
 class Hello extends Thread {
     public void run() {
         for(int i = 1; i <= 5; i++) {
-            System.out.println("Hello");
+            System.out.println("3 - Hello");
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
@@ -30,11 +43,15 @@ public class Main {
 
     public static void main(String[] args) {
         Hi obj1 = new Hi();
-        Hello obj2 = new Hello();
+        Hej obj2 = new Hej();
+        Hello obj3 = new Hello();
 
         obj1.start();
-        try { Thread.sleep(10); } catch(Exception e) {}
+        try { Thread.sleep(8); } catch(Exception e) {}
         obj2.start();
+        try { Thread.sleep(8); } catch(Exception e) {}
+        obj3.start();
+        try { Thread.sleep(8); } catch(Exception e) {}
     }
 }
 
