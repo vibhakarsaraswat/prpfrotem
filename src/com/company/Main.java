eabba0e9abb449c73bca1cc4492cefc0ed7dffbf
vibhakarsaraswat <vibhakar.saraswat@gmail.com>
2016-12-16 23:38:51 +0100

    02: MT - Multithreading using Runnable Interface

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+// Implementing THREAD using Thread class
 class Hi extends Thread {
     public void run() {
         for(int i = 1; i <= 5; i++) {
@@ -13,7 +14,7 @@ class Hi extends Thread {
     }
 }
 
-class Hej extends Thread {
+class Hej implements Runnable {
     public void run() {
         for(int i = 1; i <= 5; i++) {
             System.out.println("2 - Hej");
@@ -26,7 +27,7 @@ class Hej extends Thread {
     }
 }
 
-class Hello extends Thread {
+class Hello implements Runnable {
     public void run() {
         for(int i = 1; i <= 5; i++) {
             System.out.println("3 - Hello");
@@ -44,14 +45,24 @@ public class Main {
     public static void main(String[] args) {
         Hi obj1 = new Hi();
         Hej obj2 = new Hej();
-        Hello obj3 = new Hello();
+        /* intead of writing 'Hello obj3', we can also write 'Runnable obj3'
+          because Create the "Reference of an Interface" and "create the Object of the class".
+         */
+        Runnable obj3 = new Hello();
 
         obj1.start();
-        try { Thread.sleep(8); } catch(Exception e) {}
-        obj2.start();
-        try { Thread.sleep(8); } catch(Exception e) {}
-        obj3.start();
-        try { Thread.sleep(8); } catch(Exception e) {}
+        try { Thread.sleep(20); } catch(Exception e) { e.printStackTrace(); }
+
+        // Creating a new Thread object i.e. t1
+        // so that we can pass the Object of Runnable class i.e. obj2
+        // so that whenever 't1.start()' is called, it should execute 'run()' methood of Hej class.
+        Thread t1 = new Thread(obj2);
+        t1.start();
+        try { Thread.sleep(20); } catch(Exception e) { e.printStackTrace(); }
+
+        Thread t2 = new Thread(obj3);
+        t2.start();
+        try { Thread.sleep(20); } catch(Exception e) { e.printStackTrace(); }
+
     }
 }
-
