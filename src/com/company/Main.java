27870113e240d6a42b8dd9cbadcdd8e2076d24c6
vibhakarsaraswat <vibhakar.saraswat@gmail.com>
2016-12-17 20:05:13 +0100

    05:MT - Removed declation and implemetation of 'obj1' and 'obj2' and instead provide direct implemetation while declarion the new threads itslef i.e. t1 and t2.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,30 +3,25 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        // Creating Object 'obj1' of Runnable Interface
-        // and providing Implementation of the ANONYMOUS Class at the same time
-        // And since Runnable is a Functional Interface, we can implement Lambda expression below
-        Runnable obj1 = () -> {
+
+        // Since obj1 and obj2 are only used once,
+        // so instead of passing the OBJECT and then calling the functions,
+        // provided the function definition itself.
+        Thread t1 = new Thread(() -> {
             for(int i = 1; i <= 5; i++) {
                 System.out.println("1 - Hi");
                 try { Thread.sleep(1000); } catch (InterruptedException e) { e.printStackTrace(); }
             }
-        };
-
-        Runnable obj2 = new Runnable() {
-            public void run() {
-                for(int i = 1; i <= 5; i++) {
-                    System.out.println("2 - Hello");
-                    try { Thread.sleep(1000); } catch (InterruptedException e) { e.printStackTrace(); }
-                }
-            }
-        };
-
-        Thread t1 = new Thread(obj1);
+        });
         t1.start();
         try { Thread.sleep(20); } catch(Exception e) { e.printStackTrace(); }
 
-        Thread t2 = new Thread(obj2);
+        Thread t2 = new Thread(() -> {
+            for(int i = 1; i <= 5; i++) {
+                System.out.println("2 - Hello");
+                try { Thread.sleep(1000); } catch (InterruptedException e) { e.printStackTrace(); }
+            }
+        });
         t2.start();
         try { Thread.sleep(20); } catch(Exception e) { e.printStackTrace(); }
     }
