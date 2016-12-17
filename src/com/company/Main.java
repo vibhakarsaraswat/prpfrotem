93db94f8c38b2fdd38f1954ce83120528e45da7e
vibhakarsaraswat <vibhakar.saraswat@gmail.com>
2016-12-17 21:21:34 +0100

    07:MT - Getting and Setting Thread Names

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,27 +12,33 @@ public class Main {
                 System.out.println("1 - Hi");
                 try { Thread.sleep(1000); } catch (InterruptedException e) { e.printStackTrace(); }
             }
-        });
-        t1.start();
-        try { Thread.sleep(20); } catch(Exception e) { e.printStackTrace(); }
+        },"Thread Hi");
 
         Thread t2 = new Thread(() -> {
             for(int i = 1; i <= 5; i++) {
                 System.out.println("2 - Hello");
                 try { Thread.sleep(1000); } catch (InterruptedException e) { e.printStackTrace(); }
             }
-        });
+        },"Thread Hello");
+
+        System.out.println(t1.getName());
+        System.out.println(t2.getName());
+
+        t1.start();
+        try { Thread.sleep(20); } catch(Exception e) { e.printStackTrace(); }
+
         t2.start();
         try { Thread.sleep(20); } catch(Exception e) { e.printStackTrace(); }
 
-        // isAlive is method to check if your Thread is in Running state or Not
-        System.out.println(t1.isAlive());
+        t1.setName("This is Hi Thread");
+        t2.setName("This is Hello Thread");
+
+        System.out.println(t1.getName());
+        System.out.println(t2.getName());
 
         // Join is a method which Waits for this thread to die.
         t1.join();
         t2.join();
-
-        System.out.println(t1.isAlive());
 
         System.out.println("Bye Threads");
     }
