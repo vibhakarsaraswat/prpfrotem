c6b64501252e41996fc4ba69afc79aa1a18dde08
vibhakarsaraswat <vibhakar.saraswat@gmail.com>
2016-12-17 19:30:51 +0100

    03: MT - Multithreading using Runnable Interface and Anonymous Classes.
    Since 'Hi', and 'Hello' Classes are only used once, therefore removed these classes and instead implemented Anonymous Classes i.e.
    1. Created objects of Runnable INTERFACE and then
    2. Provided Implementation of ANONYMOUS classes for these Objects.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,68 +1,36 @@
 package com.company;
 
-// Implementing THREAD using Thread class
-class Hi extends Thread {
-    public void run() {
-        for(int i = 1; i <= 5; i++) {
-            System.out.println("1 - Hi");
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-}
-
-class Hej implements Runnable {
-    public void run() {
-        for(int i = 1; i <= 5; i++) {
-            System.out.println("2 - Hej");
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-}
-
-class Hello implements Runnable {
-    public void run() {
-        for(int i = 1; i <= 5; i++) {
-            System.out.println("3 - Hello");
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-}
-
 public class Main {
 
     public static void main(String[] args) {
-        Hi obj1 = new Hi();
-        Hej obj2 = new Hej();
-        /* intead of writing 'Hello obj3', we can also write 'Runnable obj3'
-          because Create the "Reference of an Interface" and "create the Object of the class".
-         */
-        Runnable obj3 = new Hello();
+        // Creating Object 'obj1' of Runnable Interface
+        // and providing Implementation of the ANONYMOUS Class at the same time
+        Runnable obj1 = new Runnable() {
+            public void run() {
+                for(int i = 1; i <= 5; i++) {
+                    System.out.println("1 - Hi");
+                    try { Thread.sleep(1000); } catch (InterruptedException e) { e.printStackTrace(); }
+                }
+            }
+        };
 
-        obj1.start();
-        try { Thread.sleep(20); } catch(Exception e) { e.printStackTrace(); }
+        // Creating Object 'obj2' of Runnable Interface
+        // and providing Implementation of the ANONYMOUS Class at the same time
+        Runnable obj2 = new Runnable() {
+            public void run() {
+                for(int i = 1; i <= 5; i++) {
+                    System.out.println("2 - Hello");
+                    try { Thread.sleep(1000); } catch (InterruptedException e) { e.printStackTrace(); }
+                }
+            }
+        };
 
-        // Creating a new Thread object i.e. t1
-        // so that we can pass the Object of Runnable class i.e. obj2
-        // so that whenever 't1.start()' is called, it should execute 'run()' methood of Hej class.
-        Thread t1 = new Thread(obj2);
+        Thread t1 = new Thread(obj1);
         t1.start();
         try { Thread.sleep(20); } catch(Exception e) { e.printStackTrace(); }
 
-        Thread t2 = new Thread(obj3);
+        Thread t2 = new Thread(obj2);
         t2.start();
         try { Thread.sleep(20); } catch(Exception e) { e.printStackTrace(); }
-
     }
 }
