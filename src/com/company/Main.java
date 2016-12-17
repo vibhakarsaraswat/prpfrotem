6a1dbed6f1cc801f7faddb23bf4985c217fc35a4
vibhakarsaraswat <vibhakar.saraswat@gmail.com>
2016-12-17 19:45:58 +0100

    04:MT - Multithreading, Functional Interface and Lambda expression.
    Implemented Lambda Expression only for 'Obj1'

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,17 +5,14 @@ public class Main {
     public static void main(String[] args) {
         // Creating Object 'obj1' of Runnable Interface
         // and providing Implementation of the ANONYMOUS Class at the same time
-        Runnable obj1 = new Runnable() {
-            public void run() {
-                for(int i = 1; i <= 5; i++) {
-                    System.out.println("1 - Hi");
-                    try { Thread.sleep(1000); } catch (InterruptedException e) { e.printStackTrace(); }
-                }
+        // And since Runnable is a Functional Interface, we can implement Lambda expression below
+        Runnable obj1 = () -> {
+            for(int i = 1; i <= 5; i++) {
+                System.out.println("1 - Hi");
+                try { Thread.sleep(1000); } catch (InterruptedException e) { e.printStackTrace(); }
             }
         };
 
-        // Creating Object 'obj2' of Runnable Interface
-        // and providing Implementation of the ANONYMOUS Class at the same time
         Runnable obj2 = new Runnable() {
             public void run() {
                 for(int i = 1; i <= 5; i++) {
